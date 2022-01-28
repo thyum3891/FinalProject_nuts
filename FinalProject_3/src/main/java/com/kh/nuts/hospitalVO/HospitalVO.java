6418563257868202420535febb7a3eb8f946a7c1
thyum3891 <86838449+thyum3891@users.noreply.github.com>
2022-01-28 15:22:22 +0900

    염태현 동물병원 공공데이터api 파싱

---
+++ FinalProject_3/src/main/java/com/kh/nuts/hospitalVO/HospitalVO.java
@@ -0,0 +1,45 @@
+package com.kh.nuts.hospitalVO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class HospitalVO {
+	String rnum;
+	String publisher;
+	String creator;
+	String collectionDb;
+	String title;
+	String subjectCategory;
+	String reference;
+	String source;
+	String insertDate;
+	String venue;
+	String spatial;
+	String description;
+	String subDescription;
+	String state;
+	String affiliation;
+
+//  rnum
+//	publisher
+//	creator
+//	collectionDb
+//	title
+//	subjectCategory
+//	reference
+//	source
+//	insertDate
+//	venue
+//	spatial
+//	description
+//	subDescription
+//	state
+//	affiliation
+}
